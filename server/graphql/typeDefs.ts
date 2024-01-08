181f0bd716e1cffd8fc8f270515a70dd471fdc4c
tgnlex <alexanderwebber76@gmail.com>
2024-01-08 03:31:35 -0800

    update typedefs

--- server/graphql/typeDefs.ts
+++ server/graphql/typeDefs.ts
@@ -1,20 +1,5 @@
 
 const typeDefs  = `
-
-type Event {
-  name: String!
-  date: String!
-  location: Location
-}
-type Location {
-  name: String!
-  weather: WeatherInfo
-}
-type WeatherInfo {
-  temperature: Float
-  description: String
-}
-
 type User {
   id: ID!
   nickname: String!
@@ -29,11 +14,24 @@ type Avatar {
   border: BorderColor
 }
 
+type Message {
+  time_sent: Timestamp!
+  content: String!
+  sender: User!
+  receiver: User!
+}
+
+type Timestamp {
+  date: String!
+  time: String!
+}
+
 type Post {
   id: ID!
   content: PostContent!
   owner: User  
   comments: [Comments]
+  likes: Int!
 }
 
 type Comments { 
@@ -42,6 +40,24 @@ type Comments {
   parent: Post
   owner: User
 }
+
+type Event {
+  id: ID!
+  name: String!
+  date: String!
+  location: Location
+}
+
+type Location {
+  name: String!
+  weather: WeatherInfo
+}
+
+type WeatherInfo {
+  temperature: Float
+  description: String
+}
+
 
 input CommentContent {
   body: String
@@ -87,15 +103,105 @@ type Query {
     allComments: [Comment]
 }
 
+interface MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+}
+
+type createUserMutationResponse implements MutationResponse { 
+  code: String!
+  success: String!
+  message: String!
+  user: User
+}
+
+type createPostMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  post: Post
+}
+
+type createCommentMutationResponse implements MutationResponse { 
+  code: String!
+  success: Boolean!
+  message: String!
+  comment: Comment
+}
+
+type createEventMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  event: Event
+}
+type updateEventMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  event: Event
+}
+type updateUserNicknameMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  user: User
+}
+
+type updateUserEmailMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  user: User
+}
+
+type updateUserAvatarMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  user: User
+  avatar: Avatar
+}
+
+updatePostMutationResponse implements MutationResponse {
+  code: String!
+  succesS: Boolean!
+  message: String!
+  post: Post
+}
+
+updateCommentMutationResponse implements MutationResponse { 
+  code: String!
+  success: Boolean!
+  message: String!
+  comment: Comment
+}
+
+type LikePostMutationResponse implements MutationResponse {
+  code: String!
+  success: Boolean!
+  message: String!
+  post: Post
+  user: User
+}
+
+
+
 type Mutation {
-  createUser(nickname: String!, email: String!, password: String!, avatar: Avatar)
+  createUser(nickname: String!, email: String!, password: String!, avatar: Avatar): User
   
+  updateUserNickname(id: ID!, nickname: String!): User
+  updateUserEmail(id: ID!, email: String!): User
+  updateUserAvatar(id: ID!, avatar: Avatar): User
+
   createPost(content: PostContent!): Post
   createComment(content: CommentContent!): Comment
+  createEvent(name: String!, date: String!, location: Location): Event
   
   updatePost(id: ID!, content: PostContent!): Post
   updateComment(id: ID!, content: CommentContent!) Content
-  
+  updateEventLocation(id: ID!, location: Location)
   deletePost(id: ID!): Post
   deleteComment(id: ID!): Comment
 }
